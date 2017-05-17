979c6bbc0f1fe209a53f6bfbaee4d632f786a63f
Seungjin Kim <skim@proove.com>
2017-05-17 16:18:47 -0700

    databinding-lesson: lifecycle console marking

--- other/src/app/app.component.ts
+++ other/src/app/app.component.ts
@@ -23,4 +23,8 @@ export class AppComponent {
       content: bluePrintData.serverContent
     });
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
 }
--- other/src/app/server-element/server-element.component.ts
+++ other/src/app/server-element/server-element.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {
+  Component, OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked, OnDestroy, Input, ViewEncapsulation, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,24 +12,60 @@ import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None // None, Emulated (default), Native
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked {
+  @Input('srvElement') element:{type:string, name:string, content:string};
+  @Input() name:string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
-  /*
-  Other  points:
 
-  ngOnChanges
-  ngOnInit - not rendered on page
-  ngDoCheck - change detection
-  ngAfterContentInit - called after content (ng-content) has been projected into view
-  ngAfterContentChecked - projected content has been checked for change
-  ngAfterViewInit - component's view (child views also ) has been initialized
-  ngAfterViewChecked - called every time the view (and child views) have been checked
-  ngOnDestroy - called when about to be destroyed.
+  /*
+   Other  points:
+
+   ngOnChanges
+   ngOnInit - not rendered on page
+   ngDoCheck - change detection
+   ngAfterContentInit - called after content (ng-content) has been projected into view
+   ngAfterContentChecked - projected content has been checked for change
+   ngAfterViewInit - component's view (child views also ) has been initialized
+   ngAfterViewChecked - called every time the view (and child views) have been checked
+   ngOnDestroy - called when about to be destroyed.
 
    */
+  ngOnChanges(changes:SimpleChanges) {
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
+
 }
